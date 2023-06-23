52c571be3c55fc600840dcd41e601d4151db439b
Isadora Pilau <isadorapda@gmail.com>
2023-06-23 15:19:15 +0100

    feat: 🎸 create database with faunaDB to persist user data

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,38 @@
+import { query as q } from 'faunadb'
 import { env } from '@/env'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '@/app/services/fauna'
 
 const handler = NextAuth({
-    providers: [
-            GithubProvider({
-              clientId: env.GITHUB_ID,
-              clientSecret: env.GITHUB_SECRET,
-            //   authorization:{params:{scope:"read:user"}}
-            }),
-          ],
-  })
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+      authorization: { params: { scope: 'read:user user:email' } },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(String(user.email)))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(String(user.email))))
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
+})
 
-  export { handler as GET, handler as POST }
+export { handler as GET, handler as POST }
